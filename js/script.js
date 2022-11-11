2c2e7839a4ccaedecd27833d7d2c69e5e2ac2d6c
Marek Ostrowski <rutlawski2@wp.pl>
2022-11-11 15:00:09 +0100

    Uporządkowanie funkcji w JS

--- js/script.js
+++ js/script.js
@@ -1,30 +1,38 @@
-console.log("Witajcie, drodzy programiści!");
-console.log("Zapraszam do rozejrzenia się po tym kodzie i jakiegokolwiek feedbacku =)");
+{
+    const Przywitanie = () => {
+        console.log("Witajcie, drodzy programiści!");
+        console.log("Zapraszam do rozejrzenia się po tym kodzie i jakiegokolwiek feedbacku =)");
+    }
 
-let buttonElement = document.querySelector(".js-button");
-let photoElement = document.querySelector(".js-photo");
-
-let bodyElement = document.querySelector(".js-body");
-let darkBodyElement = document.querySelector(".js-darkBody");
-let darkButtonElement = document.querySelector(".js-darkButton");
-let lightButtonElement = document.querySelector(".js-lightButton");
-
-
-buttonElement.addEventListener("click", () => {
-    photoElement.remove();
-    buttonElement.remove();
-}
-)
-
-darkButtonElement.addEventListener("click", () => {
-    bodyElement.classList.toggle("js-darkBody");
-    darkButtonElement.classList.toggle("js-lightButton");
-
-    if (bodyElement.classList.contains("js-darkBody")) {
-        darkButtonElement.innerText = "Jasny Motyw";
-    } else {
-        darkButtonElement.innerText = "Ciemny Motyw";
+    const removePhoto = () => {
+        const buttonElement = document.querySelector(".js-button");
+        const photoElement = document.querySelector(".js-photo");
+        photoElement.remove();
+        buttonElement.remove();
     };
-}
-)
 
+    const toggleBackground = () => {
+        const bodyElement = document.querySelector(".js-body");
+        const darkBodyElement = document.querySelector(".js-darkBody");
+        const lightButtonElement = document.querySelector(".js-lightButton");
+        const darkButtonElement = document.querySelector(".js-darkButton");
+        bodyElement.classList.toggle("js-darkBody");
+        darkButtonElement.classList.toggle("js-lightButton");
+
+        if (bodyElement.classList.contains("js-darkBody")) {
+            darkButtonElement.innerText = "Jasny Motyw";
+        } else {
+            darkButtonElement.innerText = "Ciemny Motyw";
+        };
+    }
+
+    const Init = () => {
+        const darkButtonElement = document.querySelector(".js-darkButton");
+        const buttonElement = document.querySelector(".js-button");
+        darkButtonElement.addEventListener("click", toggleBackground);
+        buttonElement.addEventListener("click", removePhoto);
+    }
+
+    Przywitanie();
+    Init();
+}
